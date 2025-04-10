0370fef14fcf7642888ac666d23224d2667e4bd5
Brun0HM <brunodev41@gmail.com>
2025-04-10 20:08:03 -0300

    recomeço

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import "./App.css";
-import Header from "./components/header/Header";
-import Sobre from "./components/sobre/Sobre";
 
 
 const App = () => {
   return (
     <div>
-      <Header />
-      <Sobre />
+     projeto
     </div>
   )
 }
