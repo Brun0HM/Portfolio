0af1803e7f91aa5be57936565e1b4587b9ed245f
Brun0HM <brunodev41@gmail.com>
2025-05-18 00:30:34 -0300

    finalizacao da navbar com responsividade e efeito glass e menu sanduiche na cor correta

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,10 +2,10 @@ import React from "react";
 const NavBar = (props) => {
   return (
     <>
-      <nav className="navbar navbar-expand-lg corNav">
+      <nav className="navbar navbar-expand-lg p-3 navbar-dark corNav scrolled rounded-bottom-4 position-relative">
         <div className="container-fluid">
           <button
-            className="navbar-toggler"
+            className="navbar-toggler border-0"
             type="button"
             data-bs-toggle="collapse"
             data-bs-target="#navbarSupportedContent"
@@ -16,7 +16,7 @@ const NavBar = (props) => {
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0 gap-3 mx-auto my-auto">
               <li className="nav-item">
                 <a
                   className="nav-link active text-white"
@@ -54,15 +54,14 @@ const NavBar = (props) => {
                 </a>
               </li>
             </ul>
-
-            <img
-              className="bg-white rounded-circle "
-              width="50px"
-              src={props.github}
-              alt=""
-            />
           </div>
         </div>
+        <img
+          className="github-logo-navbar"
+          width="50px"
+          src={props.github}
+          alt=""
+        />
       </nav>
     </>
   );
